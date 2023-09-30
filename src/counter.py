a9b64734cd8ac5540f9780d684e4fda08a53e3b8
Tarik Resimovic <resimovi@unlv.nevada.edu>
2023-09-30 14:48:42 -0700

    Added src/counter.py and .github/workflows/workflow.yml, updated tests/test_counter.py with new test cases

---
+++ src/counter.py
@@ -0,0 +1,31 @@
+import status
+from flask import Flask
+
+app = Flask(__name__)
+
+COUNTERS = {}
+
+# We will use the app decorator and create a route called slash counters.
+# specify the variable in route <name>
+# let Flask know that the only methods that is allowed to called
+# on this function is "POST".
+@app.route('/counters/<name>', methods=['POST'])
+def create_counter(name):
+    """Create a counter"""
+    app.logger.info(f"Request to create counter: {name}")
+    global COUNTERS
+    if name in COUNTERS:
+        return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+    COUNTERS[name] = 0
+    return {name: COUNTERS[name]}, status.HTTP_201_CREATED
+
+@app.route('/counters/<name>', methods=['PUT'])
+def update_counter(name):
+    """Update a counter"""
+    COUNTERS[name] += 1
+    return {name: COUNTERS[name]}, status.HTTP_200_OK
+
+@app.route('/counters/<name>', methods=['GET'])
+def read_counter(name):
+    """Read a counter"""
+    return {name: COUNTERS[name]}, status.HTTP_200_OK
--- tests/test_counter.py
+++ tests/test_counter.py
@@ -10,3 +10,52 @@ how to call the web service and assert what it should return.
 - The service must be able to update a counter by name.
 - The service must be able to read the counter
 """
+
+from unittest import TestCase
+
+# we need to import the unit under test - counter
+from src.counter import app 
+
+# we need to import the file that contains the status codes
+from src import status 
+
+class CounterTest(TestCase):
+    """Counter tests"""
+    
+    def test_create_a_counter(self):
+        """It should create a counter"""
+        client = app.test_client()
+        result = client.post('/counters/foo')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_duplicate_a_counter(self):
+        """It should return an error for duplicates"""
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
+
+    def test_update_a_counter(self):
+        """It should create a counter and update it"""
+        post_result = self.client.post('/counters/test')
+        self.assertEqual(post_result.status_code, status.HTTP_201_CREATED)
+        baseline_result = self.client.get('/counters/test')
+        self.assertEqual(baseline_result.status_code, status.HTTP_200_OK)
+        baseline_value = baseline_result.json
+        self.assertEqual(baseline_value['test'], 0)
+        put_result = self.client.put('/counters/test')
+        self.assertEqual(put_result.status_code, status.HTTP_200_OK)
+        updated_result = self.client.get('/counters/test')
+        self.assertEqual(updated_result.status_code, status.HTTP_200_OK)
+        updated_value = updated_result.json
+        self.assertEqual(updated_value['test'], baseline_value['test']+1)
+
+    def test_read_a_counter(self):
+        """It should create a counter and read it"""
+        post_result = self.client.post('/counters/new')
+        self.assertEqual(post_result.status_code, status.HTTP_201_CREATED)
+        get_result = self.client.get('/counters/new')
+        self.assertEqual(get_result.status_code, status.HTTP_200_OK)
